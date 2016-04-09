317b1af716262a443b257ef3191ef03f753c3f07
Michael Drury <mike12345567@live.co.uk>
2016-04-09 03:58:47 +0100

    Adding viewport meta tag to improve mobile usage and adding the correct header origins for multi client support.

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Add headers
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/", routes);
 
